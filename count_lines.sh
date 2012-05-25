a61e2b9c18a7130db772e2ec6b35ca9c2efb3ed6
Geoff Chirgwin <geoff@chirgwin.com>
2012-05-25 08:17:53 -0700

    improve accuracy of line counting script by limiting to src directory

--- count_lines.sh
+++ count_lines.sh
@@ -2,5 +2,5 @@
 # counts the number of lines in the project
 # created by chirgwin on Mon Apr 30 18:28:06 PDT 2012
 
-find * -type f -not -name ".*" | xargs wc -l
+find src/* -type f -not -name ".*" | xargs wc -l
 
